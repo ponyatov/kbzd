fb2499ebc35be72d11e9c3ffe9eb355df746993b
Dmitry Ponyatov <dponyatov@gmail.com>
2019-07-05 23:25:20 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- kb.py
+++ kb.py
@@ -1,5 +1,6 @@
 # knowledge database
-from ply.lex import lex
+
+import os,sys
 
 class Frame:
     
@@ -20,31 +21,68 @@ class Frame:
         return '\n' + '\t' * depth
     def _val(self):
         return str(self.val)
+    
+    def __getitem__(self,key):
+        return self.slot[key]
+    def __setitem__(self,key,that):
+        self.slot[key] = that ; return self
+    def __lshift__(self,that):
+        self.slot[that.val] = that ; return self
+    def __floordiv__(self,that):
+        return self.push(that)
+    
+    def push(self,that):
+        self.nest.append(that) ; return self
+    def pop(self):
+        return self.nest.pop()
 
 class Primitive(Frame): pass
 
 class Symbol(Primitive): pass
+
+class String(Primitive): pass
+
+class Active(Frame): pass
+
+class VM(Active):
+    def __setitem__(self,key,F):
+        if callable(F): self[key] = Cmd(F) ; return self
+        else: return Active.__setitem__(self,key,F)
+    def __lshift__(self,F):
+        if callable(F): return self << Cmd(F)
+        else: return Active.__lshift__(self, F)
+
+class Cmd(Active): pass
     
 import ply.lex as lex
 
 tokens = ['symbol']
 
 t_ignore = ' \t\r\n'
+t_ignore_comment = '[\#].*'
 
 def t_symbol(t):
-    r'[^ \t\r\n]+'
+    r'[`]|[^ \t\r\n\#]+'
     return Symbol(t.value)
 
 def t_error(t): raise SyntaxError(t)
 
 lexer = lex.lex()
+
+vm = VM('kb')
+
+def BYE(vm=vm): sys.exit(0)
+vm << BYE
+
+def QQ(vm=vm): print(vm) ; BYE(vm)
+vm['??'] = QQ
     
-def INTERPRET(src):
-    lexer.input(src)
+def INTERPRET(vm=vm):
+    lexer.input(vm.pop().val)
     while True:
         token = lexer.token()
         if not token: break
         print( token )
 
 if __name__ == '__main__':
-    INTERPRET(open('kb.ini').read())
+    vm // String(open('kb.ini').read()) ; INTERPRET() ; QQ()
